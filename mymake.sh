8273cb90a6e2132fb00767957c5b7447878e07e7
Gal Tsubery <gal.tsubery@gmail.com>
2026-04-04 11:45:50 -0500

    My make script

--- mymake.sh
+++ mymake.sh
@@ -2,4 +2,5 @@
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j$(nproc)
 sudo cp ./build/qbittorrent $(which qbittorrent)
-sudo ~/bin/boot.sh
+sudo setcap 'cap_net_bind_service=+eip' $(which qbittorrent)
+sudo setcap -v 'cap_net_bind_service=+eip' $(which qbittorrent)
